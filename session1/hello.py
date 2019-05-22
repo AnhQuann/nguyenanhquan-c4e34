34bd231dd9c236cd02c8f14111b8ae78e0c20f89
AnhQuann <anhquan19051905@gmail.com>
2019-05-22 21:56:33 +0700

    lan dau nop bai

---
+++ session1/hello.py
@@ -0,0 +1,10 @@
+# Day la doan code mo dau
+# name = "Dat"
+# name = "Giang"
+# print("Hello", name)
+
+# Comment code
+
+print("Enter your name: ")
+name = input()
+print("Hello", name)
---
+++ session1/turtle_intro.py
@@ -0,0 +1,11 @@
+from turtle import *
+shape("turtle")
+speed(0)
+
+for i in range(100): # for loop
+  for j in range(4):
+    forward(100)
+    left(90)
+  right(7)
+
+mainloop()
